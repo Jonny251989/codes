8200bf3e44109dc1f4d6a4b47d76dff16e07f33e
jonnyrembo <mailbox.lazarev@yandex.ru>
2025-03-02 00:36:23 +0300

    gitignore

--- qt_server/qt_game_server/server/build/CMakeFiles/Qt_game_server.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Qt_game_server.
-# This may be replaced when dependencies are built.
--- qt_server/qt_game_server/server/build/CMakeFiles/Qt_game_server_autogen.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for Qt_game_server_autogen.
